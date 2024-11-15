97ab348a232982e9e1c9db4b7bc8fd3199261c05
mutai-gilbert <mutaigilbert85@gmail.com>
2024-11-15 18:08:15 +0300

    Add the api gateway link

--- frontend/js/visitorcount.js
+++ frontend/js/visitorcount.js
@@ -3,7 +3,7 @@ async function get_visitors() {
     // call post api request function
     //await post_visitor();
     try {
-        let response = await fetch('https://i2c9mq7v59.execute-api.us-east-1.amazonaws.com/default/VisitorCounter', {
+        let response = await fetch('https://o5wh5ogvw1.execute-api.us-east-1.amazonaws.com/visitor', {
             method: 'GET',
         });
         let data = await response.json()
